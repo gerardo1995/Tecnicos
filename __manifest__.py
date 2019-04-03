acd8b661b59b73eaf1ec307dee20c16f46ef9018
gerardo1995 <gerardimar18@gmail.com>
2019-04-02 23:27:07 -0600

    otro comit

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "medic_hn",
-
+    'name': "Productos",
+    'description':"Modulo para ingresar productos"
     'category': 'Sales',
     'version': '12.0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'muk_web_theme'],
-
-    # always loaded
     'data': [
-        #'data/hn_sale_data.xml',
-        #'data/hn_sale_sequence.xml',
         'security/rules.xml',
         'security/ir.model.access.csv',
-        
-        #'wizard/consolidate_orders.xml'
-        #'report/report_layout.xml',
-        #'report/account_invoice.xml',
-        #'report/poa_report_templates.xml',
-        #'report/poa_report_pivot.xml',
-        #'report/pei_pivot.xml',
-        'views/patient.xml',
-        
-    ],
-    # only loaded in demonstration mode
-    
+        'views/patient.xml'],
 }
