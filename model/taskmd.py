b4106669c9ec53e1425aaf4cf35ac5e57f796595
kurtco67 <48306773+kurtco67@users.noreply.github.com>
2019-05-02 17:23:21 -0600

    Update taskmd.py

--- model/taskmd.py
+++ model/taskmd.py
@@ -1,8 +1,10 @@
 
 # -*- coding: utf-8 -*-
+
 from odoo import fields, models
 
 class Task(models.Model):
     _name ='technician.tarea'
     _description = 'tablad de tareas'
     name=fields.Char(string='Tarea')
+    
